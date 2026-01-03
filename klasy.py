f2712721a0744a9c7ad3c4019248588e850a859d
martakozlowskaqa-hash <martakozlowska.qa@gmail.com>
2026-01-03 11:28:58 +0100

    Added imie to czlowiek konstruktor

--- klasy.py
+++ klasy.py
@@ -3,14 +3,18 @@
 
 class Czlowiek:
     gatunek = "Homo Sapiens"
-    def __init__(self):
+    def __init__(self, imie):
     # Konstruktor
     # Akt istnienia
-        print("Niech powstanie czlowiek")
-    pass
+        print(f"Niech powstanie czlowiek o imieniu {imie}")
+        self.imie = imie
 
 # Powstawanie obiektu , gotowanie z przepisu
-adam = Czlowiek()
+adam = Czlowiek("Adam")
+ewa = Czlowiek("Ewa")
 # print(type(adam))
 # print(dir(adam))
 print(adam.gatunek)
+print(ewa.gatunek)
+print(adam.imie)
+print(ewa.imie)
